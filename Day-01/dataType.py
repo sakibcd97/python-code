85735c8e28c08abfcdf40ed30e00cd0144f712ba
sakibcd097 <sakibchowdhury0458@gmail.com>
2023-08-17 20:03:24 +0800

    update code

---
+++ Day-01/dataType.py
@@ -0,0 +1,26 @@
+
+'''
+x=3
+result=type(x)
+print(result)  #int
+
+'''
+
+'''
+id='F19010122'
+print(type(id)) #string
+
+'''
+
+
+
+
+'''
+ans=True
+print(type(ans)) #<class 'bool'>
+'''
+
+'''
+ans=5.5
+print(type(ans)) #<class 'float'>
+'''
