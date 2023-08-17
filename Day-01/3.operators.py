55d771ea42fac82641d382cf5fe117d6d09bcf53
sakibcd097 <sakibchowdhury0458@gmail.com>
2023-08-17 20:44:30 +0800

    operator update

---
+++ Day-01/3.operators.py
@@ -0,0 +1,26 @@
+
+#adding
+
+'''
+a=3
+b=2
+print(a+b)
+'''
+
+
+#Subtraction
+'''
+a=float(input('Enter your number:'))
+b=float(input('Enter your number:'))
+print(a-b)
+'''
+
+#Multiplication
+'''
+
+a=float(input('Enter your number:'))
+b=float(input('Enter your number:'))
+print(a*b)
+'''
+
+#
--- Day-02/input.py
+++
@@ -1,5 +0,0 @@
-x=float(input('Enter number:'))
-y=float(input('Enter number:'))
-
-z=x+y
-print(z)
