e9b6e7866ecffea6ccd589359848e9560a8abe99
sakibcd097 <sakibchowdhury0458@gmail.com>
2023-08-19 20:42:24 +0800

    update code

--- 3.Input-Output/3.4-multi-data-input.py
+++ 3.Input-Output/3.4-multi-data-input.py
@@ -1,6 +1,6 @@
 #Multiple ডাটার জন্য split() ব্যবহার করতে হয়
 
-number1, number2=input().split()
+number1, number2=input('Enter values:').split()
 
 result=float(number1)+float(number2)
 print(result)
