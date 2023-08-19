ab29f0adba292b5d535ba6f8b424fda9ef428386
sakibcd097 <sakibchowdhury0458@gmail.com>
2023-08-19 20:41:10 +0800

    update

--- 3.Input-Output/3.3-math-data-input.py
+++ 3.Input-Output/3.3-math-data-input.py
@@ -1,5 +1,5 @@
 a=input('Enter number-1:')
 b=input('Enter number-2:')
 
-c=float(a)+float(b)
+c=int(a)+float(b)
 print("Summation of c is:",c)
---
+++ 3.Input-Output/3.4-multi-data-input.py
@@ -0,0 +1,6 @@
+#Multiple ডাটার জন্য split() ব্যবহার করতে হয়
+
+number1, number2=input().split()
+
+result=float(number1)+float(number2)
+print(result)
