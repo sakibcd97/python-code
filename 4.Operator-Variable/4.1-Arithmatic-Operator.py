e35ec1edcd62058426bdb51459e6e7a4082c1428
sakibcd097 <sakibchowdhury0458@gmail.com>
2023-08-19 21:26:17 +0800

    update

---
+++ 4.Operator-Variable/4.1-Arithmatic-Operator.py
@@ -0,0 +1,43 @@
+
+#adding
+
+'''
+a=3
+b=2
+print(a+b)
+'''
+
+
+#Subtraction
+'''
+a=float(input('Enter your number:'))
+b=float(input('Enter your number:'))
+print(a-b)
+'''
+
+#Multiplication
+'''
+
+a=float(input('Enter your number:'))
+b=float(input('Enter your number:'))
+print(a*b)
+'''
+
+#Division	
+a=float(input('Enter your number:'))
+b=float(input('Enter your number:'))
+print("{:.2f}".format(a/b))
+print("%.2f"%round(a/b,2))
+
+
+
+#Modulus
+# a=float(input('Enter your number:'))
+# b=float(input('Enter your number:'))
+# print(a%b)
+
+
+#Exponentiation
+# a=float(input('Enter your number:'))
+# b=float(input('Enter your number:'))
+# print(a**b)
