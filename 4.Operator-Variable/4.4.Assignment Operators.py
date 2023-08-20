3440d4bc416a709d514508367bc06e386b8a3234
sakibcd097 <sakibchowdhury0458@gmail.com>
2023-08-21 00:06:24 +0800

    update

---
+++ 4.Operator-Variable/4.4.Assignment Operators.py
@@ -0,0 +1,18 @@
+number=15
+print(number)#output=15
+
+number+=5
+print(number)#output=20
+
+number-=5
+print(number)#output=15
+
+number*=5
+print(number)#output=75
+
+
+x = 5
+
+x /= 3
+
+print("{:.2f}".format(x))#output=1.67
