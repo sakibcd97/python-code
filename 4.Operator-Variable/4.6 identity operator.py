965c39ce55164ac9144531657c0e7e3037fc4584
sakibcd097 <sakibchowdhury0458@gmail.com>
2023-08-23 00:15:19 +0800

    update code

---
+++ 4.Operator-Variable/4.6 identity operator.py
@@ -0,0 +1,8 @@
+# যখন দুইটি ভেরিয়েবল এর identity same হবে তখন is ব্যবহার করলে True হবে।আর যদি same  না হয় তখন False হবে তবে এখেত্রে is not ব্যবহার করতে হবে
+
+a =12
+b=12
+name='Emon'
+
+print(a is b)
+print(a is not b)
